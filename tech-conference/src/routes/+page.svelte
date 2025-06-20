56f30fc5c81741ba15d489e1d4af663588bb8da7
Abhishek Bharatkumar Patil <abpatil122006@gmail.com>
2025-06-20 15:41:23 +0530

    Create +page.svelte

---
+++ tech-conference/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Navbar from '$lib/components/Navbar.svelte';
+  import Hero from '$lib/components/Hero.svelte';
+  import About from '$lib/components/About.svelte';
+  import Speakers from '$lib/components/Speakers.svelte';
+  import Schedule from '$lib/components/Schedule.svelte';
+  import Register from '$lib/components/Register.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<Navbar />
+<Hero />
+<About />
+<Speakers />
+<Schedule />
+<Register />
+<Footer />
